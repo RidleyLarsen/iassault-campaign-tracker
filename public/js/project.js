723524854ae584b91bb628f7efdc4b5b11bac828
Ridley Larsen <ridley@velocitywebworks.com>
2016-01-19 22:07:33 -0700

    Updated .gitignore. Removed extra OG tag from home. Fixed redirect loop.

--- public/js/project.js
+++ public/js/project.js
@@ -2,10 +2,10 @@ var ref = new Firebase(FIREBASE_URL);
 
 function handle_auth() {
   var auth = ref.getAuth();
-  if (auth === null) {
+  if (auth === null && window.location.pathname !== "/") {
     window.location.pathname = "/";
   }
-  if (auth.uid && window.location.pathname == "/") {
+  if (auth.uid && window.location.pathname === "/") {
     window.location.pathname = "/campaigns/";
   }
 }
